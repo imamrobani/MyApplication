bab8171d96eb354eed6c90f8ff6d2b5609c04506
Muhamad Rezki <muhmdrezki@gmail.com>
2017-04-12 15:44:16 +0700

    update mainActivity.java 'input void test'

--- app/src/main/java/com/example/rezki/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rezki/myapplication/MainActivity.java
@@ -10,4 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    private void test()
+    {
+        int i = 1;
+        
+    }
+    
 }
